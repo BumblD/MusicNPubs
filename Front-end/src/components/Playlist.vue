d2c79e60e460e015c78316595c2bd13c494f4c0b
Julius <siaulys.julius@gmail.com>
2020-05-20 23:30:13 +0300

    Playlist Print statements, Play/Pause button

--- Front-end/src/components/Playlist.vue
+++ Front-end/src/components/Playlist.vue
@@ -29,23 +29,22 @@
           <div v-for="s in songs" v-bind:key="s.id">
           <md-list-item>
               <span class="md-list-item-text">{{s.name}}</span>
-              <md-button class="md-icon-button md-dense md-primary">
-                <md-icon>arrow_upward</md-icon>
+              <md-button v-on:click="ArrowUp(s.id)" class="md-icon-button md-dense md-primary">
+                <md-icon >arrow_upward</md-icon>
               </md-button>
-              <md-button class="md-icon-button md-dense md-accent">
-                <md-icon>arrow_downward</md-icon>
+              <md-button v-on:click="ArrowDown(s.id)" class="md-icon-button md-dense md-accent">
+                <md-icon >arrow_downward</md-icon>
               </md-button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <md-button class="md-icon-button md-dense md-accent">
-                <md-icon>block</md-icon>
+              <md-button v-on:click="Blocked(s.id)" class="md-icon-button md-dense md-accent">
+                <md-icon >block</md-icon>
               </md-button>
               <md-button class='md-raised md-primary align-right button-down' v-on:click='Remove'>Šalinti</md-button>
             </md-list-item>
             <md-divider></md-divider>
           </div>
-
           <md-list-item>
-            <md-button class="md-fab md-mini md-primary center-horizontal">
-              <md-icon>add</md-icon>
+            <md-button v-on:click="CreateNew" class="md-fab md-mini md-primary center-horizontal">
+              <md-icon >add</md-icon>
             </md-button>
           </md-list-item>
           <md-divider></md-divider>
@@ -54,19 +53,20 @@
 
       <div class="md-layout-item md-layout md-gutter">
         <div class="md-layout-item">
-          <md-button class="md-fab md-primary">
-            <md-icon>skip_previous</md-icon>
+          <md-button v-on:click="SkipPrevious" class="md-fab md-primary">
+            <md-icon >skip_previous</md-icon>
           </md-button>
-          <md-button class="md-fab md-primary">
-            <md-icon>play_arrow</md-icon>
+          <md-button v-on:click="Play" class="md-fab md-primary">
+            <md-icon v-show="PlayDisplay">play_arrow</md-icon>
+            <md-icon v-show="!PlayDisplay">stop</md-icon>
           </md-button>
-          <md-button class="md-fab md-primary">
-            <md-icon>skip_next</md-icon>
+          <md-button v-on:click="SkipNext" class="md-fab md-primary">
+            <md-icon >skip_next</md-icon>
           </md-button>
         </div>
         <div class="md-layout-item">
-          <md-button class="md-fab md-primary">
-            <md-icon>shuffle</md-icon>
+          <md-button v-on:click="Shuffle"class="md-fab md-primary">
+            <md-icon >shuffle</md-icon>
           </md-button>
         </div>
       </div>
@@ -86,6 +86,7 @@ export default {
   name: 'App',
   data: function () {
     return {
+      PlayDisplay: false,
       LetChoose: false,
       barId: 1,
       playlistName: 'European hits',
@@ -95,21 +96,50 @@ export default {
     }
   },
   methods: {
+    Shuffle: function()
+    {
+      console.log("Shuffle");
+    },
+    Play: function()
+    {
+      this.PlayDisplay = !this.PlayDisplay;
+      console.log("Play" + this.PlayDisplay);
+    },
+    SkipPrevious: function()
+    {
+      console.log("SkipPrevious");
+    },
+    SkipNext: function()
+    {
+      console.log("SkipNext");
+    },
+    CreateNew: function()
+    {
+      console.log("CreateNew");
+    },
+    ArrowDown: function(i)
+    {
+      console.log("ArrowDown"+i);
+    },
+    ArrowUp: function(i)
+    {
+      console.log("ArrowUp"+i);
+    },
+    Blocked: function(i)
+    {
+      console.log("blocked"+i);
+    },
     Remove: function()
     {
       console.log("removed");
-    },
-    work: function()
-    {
-      console.log("hi");
     },
     LoadPlaylists: async function () {
      /* await axios.get('https://localhost:44341/api/playlist/getbarplaylists/' + this.barId)
         .then(response => (this.playlists = response.data))*/
     },
     LoadPlaylistSongs: async function () {
-      await axios.get('https://localhost:44341/api/playlist/getplaylistsongs/' + this.playlist.id)
-        .then(response => (this.songs = response.data))
+      /*await axios.get('https://localhost:44341/api/playlist/getplaylistsongs/' + this.playlist.id)
+        .then(response => (this.songs = response.data))*/
     }
   },
   async beforeMount () {
